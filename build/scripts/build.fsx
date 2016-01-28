0d50259e31e0c17cf142b1ac45e4b2ea7f667b68
Daniel Müller <mail@dotnetnukeblog.de>
2016-01-28 12:13:36 +0100

    Add fake and fxcop for build and release management

---
+++ build/scripts/build.fsx
@@ -0,0 +1,57 @@
+// include Fake lib
+#r "../../packages/FAKE/tools/FakeLib.dll"
+
+open Fake
+
+// Directories
+let outDir = "./out"
+let buildDir  = outDir + "/build/"
+let testDir   = outDir + "/test/"
+let nugetDir  = outDir + "/nuget/"
+
+// tools
+let fxCopRoot = "build/Tools/FxCop/FxCopCmd.exe"
+
+
+Target "Clean" (fun _ -> 
+    CleanDirs ["obj"; buildDir; testDir; nugetDir]
+)
+
+Target "PrepareBuild" (fun _ ->
+    CreateDir buildDir
+    CreateDir testDir
+    CreateDir nugetDir
+)
+
+Target "compile" (fun _ ->
+   !! "./src/**/*.csproj"
+    |> MSBuildRelease buildDir "Build"
+    |> Log "AppBuild-Output: *"
+ )
+
+Target "FxCop" (fun _ ->
+    !! (buildDir + "/**/*.dll")
+        |> FxCop (fun p ->
+            {p with
+                ReportFileName = testDir + "FXCopResults.xml"
+                ToolPath = fxCopRoot})
+)
+
+Target "CreateNuGetPackage"(fun _ ->
+    NuGet (fun p -> 
+       {p with
+            Version = "1.0.0.0"
+            OutputPath = nugetDir
+            WorkingDir = @"."
+            Publish = false })
+        "./build/nuget/dng.ResumableUpload.nuspec"
+)
+
+"PrepareBuild"
+    ==> "Clean"
+    ==> "compile"
+    ==> "FxCop"
+    ==> "CreateNuGetPackage"
+
+// start build
+RunTargetOrDefault "CreateNuGetPackage"
